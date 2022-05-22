2a44a6be33ff4dae7154a01bf680b8eaa4ab0958
Ismail Gadzhiev <ismailgadzhievff@gmail.com>
2022-05-22 16:26:24 +0300

    refactoring, close #23

--- scripts/models/_models.py
+++ scripts/models/_models.py
@@ -3,4 +3,4 @@ from .factory import SklearnModelFactory, KerasModelFactory
 sk_model_factory = SklearnModelFactory()
 nn_model_factory = KerasModelFactory()
 
-from .model_sklearn import SkLearnModels, KerasModels
+from .models import SkLearnModels, KerasModels
